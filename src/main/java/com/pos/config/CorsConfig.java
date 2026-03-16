8c86c0917d923d4dc86adafd0ccb15bc9479f045
irvanfahreza <irvanfahreza45@gmail.com>
2026-03-16 11:40:45 +0700

    cors v3

--- src/main/java/com/pos/config/CorsConfig.java
+++ src/main/java/com/pos/config/CorsConfig.java
@@ -2,11 +2,8 @@ package com.pos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
